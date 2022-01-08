7fd876b82111ad36581ef8d218250cdee797ed87
Bryan Liles <bryanliles@gmail.com>
2022-01-08 11:49:54 -0500

    add photo-cloudinit module

---
+++ photon-cloudinit/variables.tf
@@ -0,0 +1,31 @@
+variable "instance_id" {
+  type        = string
+  description = "The vCenter instance id"
+}
+
+variable "local_hostname" {
+  type        = string
+  description = "The hostname for the instance"
+}
+
+variable "ssh_pub_key_path" {
+  type        = string
+  description = "Path to an SSH publick key"
+}
+
+variable "encrypted_root_password" {
+  type        = string
+  description = "Encrypted root password"
+}
+
+variable "packages" {
+  description = "Packages to install"
+  type        = list(string)
+  default     = []
+}
+
+variable "additional_userdata" {
+  description = "Additional userdata configuration as a string"
+  type        = string
+  default     = ""
+}
