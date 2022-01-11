084646af2b062543f528f77e5a257f2289f4cf6b
Bryan Liles <bryanliles@gmail.com>
2022-01-10 19:05:20 -0500

    add vsphere-cloudinit module
    This replaces photon-cloudinit

---
+++ vsphere-cloudinit/variables.tf
@@ -0,0 +1,37 @@
+variable "instance_id" {
+  type        = string
+  description = "The vCenter instance id"
+}
+
+variable "local_hostname" {
+  type        = string
+  description = "The hostname for the instance"
+}
+
+variable "interface" {
+  type        = string
+  description = "network interface"
+  default     = "eth0"
+}
+
+variable "ssh_pub_key_path" {
+  type        = string
+  description = "Path to an SSH publick key"
+}
+
+variable "encrypted_root_password" {
+  type        = string
+  description = "Encrypted root password"
+}
+
+variable "packages" {
+  description = "Packages to install"
+  type        = list(string)
+  default     = []
+}
+
+variable "additional_userdata" {
+  description = "Additional userdata configuration as a string"
+  type        = string
+  default     = ""
+}
