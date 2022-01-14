ff8a447245bd5a0e2a6dbd995ca012883ef2c176
Bryan Liles <bryanliles@gmail.com>
2022-01-13 21:35:53 -0500

    remove photon specific modules

--- photon-vm/README.md
+++
@@ -1,7 +0,0 @@
-# photon-vm
-
-Requirements:
-
-* Configured vsphere provider
-
-Create a photon VM in vCenter.
--- photon-vm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "default_ip_address" {
-  value = vsphere_virtual_machine.vm.default_ip_address
-}
-
-output "name" {
-  value = vsphere_virtual_machine.vm.name
-}
